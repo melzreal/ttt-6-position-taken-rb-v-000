0a7df626d7818d4202a1dc30eecc0956bc9eeb97
Melissa Real <melissa.real@hotmail.com>
2018-04-10 18:09:21 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,9 +4,9 @@ def position_taken(board,index)
   
 def position_taken(board,index)
   
-  if (board[index]==" " || board[index]=="" || board[index]== nil)
+  if (board[index]==" " || board[index]=="")
     false
-  else (board[index]=="X" || board[index]=="O")
+  true (board[index]=="X" || board[index]=="O")
     true
   end
 
