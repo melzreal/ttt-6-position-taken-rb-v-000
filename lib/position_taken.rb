567a508735202cbc4ed238398e262409e56040f4
Melissa Real <melissa.real@hotmail.com>
2018-04-10 18:12:11 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 # code your #position_taken? method here!
 
-
-def position_taken(board,index)
-  
+def position_taken?(board,index)
   if (board[index]==" " || board[index]=="")
     false
   else (board[index]=="X" || board[index]=="O")
