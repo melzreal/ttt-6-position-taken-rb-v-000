a08ab2acffef1888e45aa0e85609b05d64e0171b
Melissa Real <melissa.real@hotmail.com>
2018-04-10 18:10:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,13 @@
 # code your #position_taken? method here!
 
+
 def position_taken(board,index)
   
-def position_taken(board,index)
-  
-  if (board[index]==" " || board[index]=="" || board[index]== nil)
+  if (board[index]==" " || board[index]=="")
     false
   else (board[index]=="X" || board[index]=="O")
     true
   end
 
-end
+
 end
